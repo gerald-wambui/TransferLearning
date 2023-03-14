8f6f03762b1ae04894617daa08f8825c1e083b03
Gerald <geraldwainaina25@gmail.com>
2023-03-14 21:28:19 +0300

    Update tf_flowers.py

--- tf_flowers.py
+++ tf_flowers.py
@@ -5,5 +5,15 @@ import tensorflow_hub as hub
 import tensorflow_datasets as tfds
 from tensorflow.keras import layers
 import logging
+
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
+
+# Download the dataset
+(training_set, validation_set), dataset_info = tfds.load(
+    'tf_flowers',
+    split=['train[:70%]', 'train[70%:]'],
+    with_info=True,
+    as_supervised=True,
+)
+
