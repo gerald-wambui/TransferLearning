3a1ac0f3cb480f61866bd2442e98d30745785cbf
Gerald <geraldwainaina25@gmail.com>
2023-03-18 17:41:41 +0300

    Update tf_flowers.py

--- tf_flowers.py
+++ tf_flowers.py
@@ -89,5 +89,16 @@ val_acc = history.history['val_accuracy']
 
 loss = history.history['loss']
 val_loss = history.history['val_loss']
+
+epochs_range = range(EPOCHS)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
 # fucked
 # feeling confused abt this give me time
